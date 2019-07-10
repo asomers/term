cc57c16753fdd1fb3adb4e35de7238cac3f6c311
Alan Somers <asomers@gmail.com>
2019-07-10 08:57:38 -0600

    Add examples/prterm
    This example prints the capabilities of the currently connected terminal
    and tries a few operations, the same operations that Rustup needs.

---
+++ examples/prterm.rs
@@ -0,0 +1,57 @@
+extern crate term;
+
+fn main() {
+    // First, print a description of the current terminal
+    let terminfo = term::terminfo::TermInfo::from_env();
+    dbg!(&terminfo);
+
+    // Secondly, try using the methods that rustup needs.  If the output looks
+    // correct then the terminal really works.
+    let mut t = term::stdout().unwrap();
+
+    if t.supports_color() {
+        t.fg(term::color::GREEN).unwrap();
+        writeln!(t, "Foreground green").unwrap();
+        t.reset().unwrap();
+
+        t.bg(term::color::GREEN).unwrap();
+        writeln!(t, "Background green").unwrap();
+        t.reset().unwrap();
+    } else {
+        writeln!(t, "Color not supported").unwrap();
+    }
+
+    if let Ok(_) = t.attr(term::Attr::Bold) {
+        writeln!(t, "Bold!").unwrap();
+        t.reset().unwrap();
+    } else {
+        writeln!(t, "Bold not supported").unwrap();
+    }
+
+    write!(t, "overwrite").unwrap();
+    if let Ok(_) = t.carriage_return() {
+        writeln!(t, "overwrite supported").unwrap();
+    } else {
+        writeln!(t, " not supported").unwrap();
+    }
+
+    writeln!(t, "cursor up").unwrap();
+    if let Ok(_) = t.cursor_up() {
+        writeln!(t, "cursor up supported").unwrap();
+    } else {
+        writeln!(t, "          not supported").unwrap();
+    }
+
+    write!(t, "delete line").unwrap();
+    if let Ok(_) = t.carriage_return() {
+        if let Ok(_) = t.delete_line() {
+            writeln!(t, "delete line supported").unwrap();
+        } else {
+            writeln!(t, " not supported").unwrap();
+        }
+    } else {
+        writeln!(t, " is pointless without carriage return").unwrap();
+    }
+
+    t.reset().unwrap();
+}
